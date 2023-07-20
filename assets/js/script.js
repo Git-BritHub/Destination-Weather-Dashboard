cefbe579b50bf6ae1c6e2f93633acf51d4aea353
Brittany Brimley <brittanybrimley@brittanys-mbp.lan>
2023-07-20 16:47:45 -0600

    set up js to pull data and display on page for hardcoded location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var key = "aab85c305da960a0909bc03c76c938a2";
-`http://api.openweathermap.org/data/2.5/forecast?q=austin&appid=${key}&units=imperial`
-
+var searchInfo = document.querySelector(".searchInfo")
+var currentIcon = document.querySelector(".currentIcon")
+var currentTemp = document.querySelector(".currentTemp")
+var currentHumidity = document.querySelector(".currentHumidity")
+var currentWind = document.querySelector(".currentWind")
 
 function getData() {
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=austin&appid=${key}&units=imperial`)
@@ -9,6 +12,18 @@ function getData() {
         })
         .then(function (data) {
             console.log(data)
+            var date = new Date()
+            var day = date.getDate()
+            var month = date.getMonth() + 1
+            var year = date.getFullYear()
+            var buildDate = month + "/" + day + "/" + year
+            searchInfo.innerHTML = data.name + " - " + buildDate;
+            // Math.round() rounds  nearest whole number 104.9 => 105
+            currentTemp.innerHTML = "Temperature: " + Math.round(data.main.temp) + "&#176F";
+            currentHumidity.innerHTML = "Humidity: " + Math.round(data.main.humidity) + "%";
+            currentWind.innerHTML = "Wind speed: " + Math.round(data.wind.speed) + "mph";
+            currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+            currentIcon.setAttribute("alt", data.weather[0].description)
         })
         .catch(function (err) {
             console.log(err)
