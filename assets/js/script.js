c10a77ea5c939c0bf9e4c8bd30d6ba6476ef6356
Brittany Brimley <brittanybrimley@brittanys-mbp.lan>
2023-07-28 13:06:10 -0600

    added function for previously typed locations to store in local storage and have corresponding button

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,35 @@ var currentHumidity = document.querySelector(".currentHumidity")
 var currentWind = document.querySelector(".currentWind")
 var searchInfo = document.querySelector("#search")
 var searchInput = document.querySelector("#city")
+var historyList = document.querySelector(".history")
 
 var forecastCard = document.querySelectorAll(".card-forecast")
 
-function getData(event) {
+var searchHistory = JSON.parse(localStorage.getItem("history")) || []
+
+function fetchSearchHistory() {
+    var data = JSON.parse(localStorage.getItem("history"))
+    historyList.innerHTML = ""
+    if (data) {
+        for (let i = 0; i < data.length; i++) {
+            var btn = document.createElement("button")
+            btn.innerHTML = data[i]
+            btn.addEventListener("click", function(event) {
+                getData(event, event.target.innerHTML)
+            })
+            historyList.append(btn)
+        }
+    } else {
+        return;
+    }
+}
+
+fetchSearchHistory()
+
+function getData(event, city) {
     // fetch api call on weather api
     event.preventDefault()
-    var city = searchInput.value.trim()
-    console.log(city);
-
+    
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`)
         .then(function (res) {
             // if we forget to response.json it, it wont allow us to pull all the api's info when we call the function getData()
@@ -42,21 +62,22 @@ function getData(event) {
         .catch(function (err) {
             console.log(err)
         })
-
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`)
+        
+        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`)
         .then(function (res) {
             return res.json()
         })
         .then(function (data) {
             console.log(data)
-            for(let i = 0; i < forecastCard.length; i++) {
+            for (let i = 0; i < forecastCard.length; i++) {
+                forecastCard[i].innerHTML = ""
                 var listIndex = i * 8 + 4;
                 var date = new Date(data.list[listIndex].dt * 1000)
                 var day = date.getDate()
                 var month = date.getMonth() + 1
                 var year = date.getFullYear()
                 var dateEl = document.createElement("h5")
-                dateEl.innerHTML =  month + "/" + day + "/" + year
+                dateEl.innerHTML = month + "/" + day + "/" + year
                 forecastCard[i].append(dateEl)
                 var icon = document.createElement("img")
                 icon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[listIndex].weather[0].icon + "@2x.png")
@@ -71,13 +92,19 @@ function getData(event) {
                 var windEl = document.createElement("p")
                 windEl.innerHTML = "Wind: " + Math.round(data.list[listIndex].wind.speed) + "mph"
                 forecastCard[i].append(windEl)
-
+                
             }
         })
         .catch(function (err) {
             console.log(err)
         })
-}
-
+    }
+    
 // used submit as opposed to click to save lines of code by changing search section to form in html
-searchInfo.addEventListener("submit", getData);
+searchInfo.addEventListener("submit", function (event) {
+    var city = searchInput.value.trim()
+    searchHistory.push(city)
+    localStorage.setItem("history", JSON.stringify(searchHistory))
+    getData(event, city)
+    fetchSearchHistory()
+})
